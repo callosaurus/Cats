e6d03207612df58c7c3ea8fe42042e03b7daaa6a
callosaurus <callumdavies421@gmail.com>
2017-02-27 14:09:11 -0800

    first commit

---
+++ Cats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cats
+//
+//  Created by Callum Davies on 2017-02-27.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cats/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cats
+//
+//  Created by Callum Davies on 2017-02-27.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
